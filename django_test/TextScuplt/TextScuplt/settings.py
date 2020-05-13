887409c15cbc8cdf89d5b06976eb4b658883d08e
Hanson Zhao <zhaohanson1@berkeley.edu>
2020-05-12 17:45:02 -0700

    Initialize word features and JS skeleton code

--- django_test/TextScuplt/TextScuplt/settings.py
+++ django_test/TextScuplt/TextScuplt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'word_features.apps.WordFeaturesConfig'
 ]
 
 MIDDLEWARE = [
---
+++ django_test/TextScuplt/word_features/static/word_features/js/word_functions.js
@@ -0,0 +1,32 @@
+
+function wordCount(inputString) {
+    /**
+     * Count the number of words.
+     * @param {string}  inputString     The input text
+     * @return {int}    The number of words in inputString
+     */
+
+    // TODO
+    return 0;
+}
+
+function wordDiff(input1, input2) {
+    /**
+     * Return the index ranges where the text differs
+     * @param {string} input1       The first text
+     * @param {string} input2       The second text
+     * @return {Array.<Array>}      A list of ranges
+     */
+    return 0;
+}
+
+function wordReplace(inputString, toReplace, replace){
+    /**
+     * Replace a character pattern with some other characters
+     * @param {string} inputString      The input text
+     * @param {object} toReplace        The regex pattern to replace
+     * @param {string} replace          The replacement string
+     * @return {string}     The resulting string
+     */
+    return '';
+}

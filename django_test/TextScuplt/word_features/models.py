96e7dce233d16e7d9387ca6a88e64d6c538546a7
Hanson Zhao <zhaohanson1@berkeley.edu>
2020-05-17 21:58:15 -0700

    starting tool view page

--- django_test/TextScuplt/word_features/models.py
+++ django_test/TextScuplt/word_features/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class ToolMetadata(models.Model):
+    tool_name = models.CharField(max_length=30)
+    description = models.CharField(max_length=140)
--- django_test/TextScuplt/word_features/views.py
+++ django_test/TextScuplt/word_features/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("HELLO WORLD")
+    return render(request, 'word_features/index.html')

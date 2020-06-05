e912e3fa5282b0b0dc067150e421b029772ed8cd
Hanson Zhao <zhaohanson1@gmail.com>
2020-06-04 17:10:35 -0700

    Update diff_controller.js
    changed 'var' to 'let'

--- django_test/TextScuplt/word_features/static/word_features/js/diff_controller.js
+++ django_test/TextScuplt/word_features/static/word_features/js/diff_controller.js
@@ -1,9 +1,9 @@
-var lineDiff = new LineDiff();
-var text1 = $('#id_text_1')
-var text2 = $('#id_text_2')
-var rangeInput = $('#id_ranges')
+let lineDiff = new LineDiff();
+let text1 = $('#id_text_1')
+let text2 = $('#id_text_2')
+let rangeInput = $('#id_ranges')
 
 $('#diff-form').submit(function() {
-    var r = lineDiff.diff(text1.val(), text2.val());
+    let r = lineDiff.diff(text1.val(), text2.val());
     rangeInput.val(JSON.stringify(r));
 });

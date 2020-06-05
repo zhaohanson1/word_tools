cfd1a4935abcb0a9e006998d426732177cf4faab
Hanson Zhao <zhaohanson1@berkeley.edu>
2020-06-05 16:41:56 -0700

    refactor and styled

--- django_test/TextScuplt/word_features/forms.py
+++ django_test/TextScuplt/word_features/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 
 class DiffForm(forms.Form):
+    textarea_attr = {
+        'rows': 8,
+        'autocomplete': 'off',
+        'wrap': 'off'
+        }
+        
     text_1 = forms.CharField(label='Text 1', 
-                            widget=forms.Textarea, 
+                            widget=forms.Textarea(attrs=textarea_attr),
                             required=False)
     text_2 = forms.CharField(label='Text 2', 
-                            widget=forms.Textarea, 
+                            widget=forms.Textarea(attrs=textarea_attr),
                             required=False)
     ranges = forms.CharField(widget=forms.HiddenInput)

5918831d491de3e55bc8ba2d7bbaca1ef19d0d6b
Hanson Zhao <zhaohanson1@berkeley.edu>
2020-06-09 02:17:35 -0700

    modified key

--- django_test/TextScuplt/config/settings/production.py
+++ django_test/TextScuplt/config/settings/production.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('prod_secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'I_AM_A_DUMMY_KEY_CHANGE_ME'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True

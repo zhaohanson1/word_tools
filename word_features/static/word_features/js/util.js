8c0d974824b7c61e6429ac9be92f43103eb402d7
Hanson Zhao <zhaohanson1@berkeley.edu>
2021-06-10 02:19:28 -0700

    Paste button and refactored copy button

--- word_features/static/word_features/js/util.js
+++ word_features/static/word_features/js/util.js
@@ -1,24 +1,55 @@
 // Initalize all popovers
-$(function () {
+$(function() {
     $('[data-toggle="tooltip"]').tooltip();
-  })
+})
 
 const copyTextAreaToClipboard = function(e) {
     let id = e.target.dataset.textareaName;
-    $('#'+id).select();
-    document.execCommand('copy');
+    var textArea = document.getElementById(id);
+
+    if (!navigator.clipboard) {
+        $('#' + id).select();
+        document.execCommand('copy');
+    } else {
+        var text_to_copy = textArea.value;
+        navigator.clipboard.writeText(text_to_copy).catch(
+            function(err) {
+                console.error("err in copy")
+                console.error(err);
+            }
+        );
+    }
     document.getSelection().empty();
-  };
+};
 
+const pasteClipboardToTextArea = function(e) {
+    let id = e.target.dataset.textareaName;
+    var textArea = document.getElementById(id);
+    if (!navigator.clipboard) {
+        $('#' + id).select();
+        document.execCommand('paste');
+    } else {
+        textArea.focus();
+        navigator.clipboard.readText().then(
+            t => textArea.value = t
+        ).catch(
+            function(err) {
+                console.error("err in paste")
+                console.error(err);
+            }
+        );
+    }
+    document.getSelection().empty();
+};
 
-let storedText = null; 
-swapText = function(textbox1, textbox2) {
+let storedText = null;
+const swapText = function(textbox1, textbox2) {
     storedText = textbox1.value;
     textbox1.value = textbox2.value;
     textbox2.value = storedText;
 }
 
-undoText = function(textbox1) {
+const undoText = function(textbox1) {
     if (storedText)
-      textbox1.value = storedText;
+        textbox1.value = storedText;
 }
